43364383eb1834df7fd6fdc86dced4a7fbdee2cb
Oleksandr Makarevich <oleksandr.makarevich@oracle.com>
2020-05-22 19:42:56 +0300

    added changes in text setion

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Was changed ! 
+          My new application ! 
         </p>
         <a
           className="App-link"
